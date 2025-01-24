71dd056265e30b9aca9f9cdc240cbdb83b3a991d
Anurup Jaiswal <anurupjaiswal007@gmail.com>
2025-01-24 15:51:01 +0530

    intricate the OderHistory Api

---
+++ app/src/main/java/com/anurupjaiswal/learnandachieve/model/OrderHistoryResponse.kt
@@ -0,0 +1,16 @@
+package com.anurupjaiswal.learnandachieve.model
+
+data class OrderHistoryResponse(
+    val message: String,                // "Success"
+    val data: List<Order>,              // List of orders
+    val availableDataCount: Int         // The total number of orders available
+)
+
+data class Order(
+    val order_id: String,               // "678634c98a60f3272586eea0"
+    val orderId: String,                // "ORD-20250114-60181"
+    val packageName: String,            // "Class 12th Package Test"
+    val date: String,                   // "14 January, 2025"
+    val transaction_id: String,         // "TXN-20250114-b62844d9d50de0da"
+    val amount: String                  // "4000.00"
+)
